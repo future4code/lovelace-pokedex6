88b2a43d68bb1e91aa00a0559db8777c301689ff
Cleiton Silva Soares <cleiton.ugo@hotmail.com>
2021-08-24 16:23:36 -0300

    objeto de detalhes dos pokemons formatado

--- pokedex/src/components/Card/index.js
+++ pokedex/src/components/Card/index.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import Pokemonzinho from "../../img/pokemonzinho.jpg"
+import Pokemonzinho from '../../img/pokemonzinho.jpg'
 import { PokemonCard } from './styles'
 
-const Card = () => {
-    return (
-        <PokemonCard>
-            <h3>Bulbassauro</h3>
-            <img src={Pokemonzinho} alt="Bulbassauro"/>
-            <div> 
-            <button>Adicionar à Pokedex</button>
-            <button>Mais detalhes</button>
-            </div>
-
-        </PokemonCard>
-    )
+const Card = (props) => {
+  return (
+    <PokemonCard>
+      {<h3>{props.pokemon.name}</h3>}
+      <img src={Pokemonzinho} alt='Bulbassauro' />
+      <div>
+        <button>Adicionar à Pokedex</button>
+        <button>Mais detalhes</button>
+      </div>
+    </PokemonCard>
+  )
 }
 
 export default Card
--- pokedex/src/pages/Home/index.js
+++ pokedex/src/pages/Home/index.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Card from '../../components/Card/index'
+import useGetPokemons from '../../services/useGetPokemons'
+import useGetDetails from '../../services/useGetDetails'
 
 const Home = () => {
+  const { pokemons, getPokemons } = useGetPokemons()
+  const { details, getDetails } = useGetDetails()
+  const [pokemonDetails, setPokemonDetails] = useState([])
+
+  useEffect(() => {
+    getPokemons()
+    getDetails('3')
+  }, [])
+
+  details && console.log(details)
+
+  // const detalhes = pokemons.map((pokemon) => {
+  //   getDetails(pokemon.url)
+  //   return details
+  // })
+
+
+  const mostraPokemons = pokemons.map((pokemon) => {
+    return <Card pokemon={pokemon} />
+
+  })
+
+
+  // pokemons && console.log(pokemons)
+
   return (
     <div>
-      Lista de pokemons
+      {/* <Card pokemon={pokemons[6]} /> */}
+      {mostraPokemons}
     </div>
   )
 }
