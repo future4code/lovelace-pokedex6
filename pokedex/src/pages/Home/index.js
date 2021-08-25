d9b097ab1ee828cc38c1a10f675a9e6d965a4268
cleitonsoares-s <cleiton.ugo@hotmail.com>
2021-08-25 16:59:33 -0300

    headers pronto

--- pokedex/src/pages/Home/index.js
+++ pokedex/src/pages/Home/index.js
@@ -3,7 +3,12 @@ import Card from '../../components/Card/index'
 import useGetPokemons from '../../services/useGetPokemons'
 import useGetDetails from '../../services/useGetDetails'
 import { PokemonGrid } from './styles'
+import Header from '../../components/Header'
+import { useHistory } from 'react-router-dom'
+
 const Home = () => {
+  const history = useHistory()
+
   const { pokemons, getPokemons } = useGetPokemons()
   const { details, getDetails } = useGetDetails()
 
@@ -12,12 +17,18 @@ const Home = () => {
   }, [])
 
   return (
-    <PokemonGrid>
-      
-      {pokemons.map((pokemon) => (
-        <Card key={pokemon} pokemon={pokemon} />
-      ))}
-    </PokemonGrid>
+    <>
+      <Header
+        buttonName='Ir para Pokédex'
+        title='Lista de Pokémons'
+        onClickButton={() => history.push('/pokedex')}
+      />
+      <PokemonGrid>
+        {pokemons.map((pokemon) => (
+          <Card key={pokemon} pokemon={pokemon} />
+        ))}
+      </PokemonGrid>
+    </>
   )
 }
 
